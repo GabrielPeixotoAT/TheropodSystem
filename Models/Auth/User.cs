7de30bf78072c4380968fb4103f0111768ec7dce
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-02-23 19:52:50 -0300

    fix: prorpiesades da classe User aceitam valores nulos

--- Models/Auth/User.cs
+++ Models/Auth/User.cs
@@ -7,8 +7,8 @@ namespace TheropodSystem.Models.Auth
 {
     public class User
     {
-        public string Name { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
     }
 }
